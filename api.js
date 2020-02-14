75866ee1f57db0f71e9d66f24340a95fa5f25b4f
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-14 11:02:37 +0300

    updated api.js and index.html js imports

--- api.js
+++ api.js
@@ -1,15 +1,28 @@
 var $api = {
+    __xmlhttp: undefined,
+    __url: 'https://www.bloomberght.com/piyasa/intradaydata/dolar',
     initialize: function() {
         $.ajaxSetup({
             type: 'GET',
-            dataType: 'json',
-            url: 'https://www.bloomberght.com/piyasa/intradaydata/dolar',
+            url: 'https://cors-anywhere.herokuapp.com/https://www.bloomberght.com/piyasa/intradaydata/dolar',
             success: function(data) {
-                $dom.updateData(data)
+                $dom.updateData(JSON.parse(data))
             }
         })
+        console.log("API is initialized.")
+        //$api.__xmlhttp = new XMLHttpRequest()
+        //$api.__xmlhttp.withCredentials = true
+        //$api.__xmlhttp.onreadystatechange = function() {
+        //    if (this.readyState == 4 && this.status == 200) {
+        //        $dom.updateData(JSON.parse(this.responseText))
+        //    } else {
+        //        alert("Err: state: " + this.readyState + ", status: " + this.status)
+        //    }
+        //};
     },
     get: function() {
         $.ajax()
+        //$api.__xmlhttp.open("GET", $api.__url, true)
+        //$api.__xmlhttp.send()
     }
 }
